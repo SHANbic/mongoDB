4b911bbf74741e37823c1cbcf4e59a892f70c06b
Pierre <pierre.lam91@gmail.com>
2019-08-26 22:25:39 +0200

    [ FEATURE ] Added unit tests with model class update methods

--- test/update_test.js
+++ test/update_test.js
@@ -9,15 +9,34 @@ describe('Updating records', () => {
     joe.save().then(() => done());
   });
 
-  it('instance type using set and save', done => {
-    joe.set('name', 'Alex');
-    joe
-      .save()
+  function assertName(operation, done) {
+    operation
       .then(() => User.find({}))
       .then(users => {
         assert(users.length === 1);
         assert(users[0].name === 'Alex');
         done();
       });
+  }
+
+  it('instance type using set and save', done => {
+    joe.set('name', 'Alex');
+    assertName(joe.save(), done);
+  });
+
+  it('a model instance can update', done => {
+    assertName(joe.update({ name: 'Alex' }), done);
+  });
+
+  it('a model class can update', done => {
+    assertName(User.update({ name: 'Joe' }, { name: 'Alex' }), done);
+  });
+
+  it('a model class can update one record', done => {
+    assertName(User.findOneAndUpdate({ name: 'Joe' }, { name: 'Alex' }), done);
+  });
+
+  it('a model class can find a record with an Id and update', done => {
+    assertName(User.findByIdAndUpdate(joe._id, { name: 'Alex' }), done);
   });
 });
