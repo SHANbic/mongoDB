9b57b5ef9fc781a8b42f1d03d6329c4234617c0a
Pierre <pierre.lam91@gmail.com>
2019-08-29 13:15:38 +0200

    [ FEATURE ] Implemented a unit test to check if a user as a blogpost

--- test/association_test.js
+++ test/association_test.js
@@ -3,6 +3,29 @@ const User = require('../src/user');
 const Comment = require('../src/comment');
 const BlogPost = require('../src/blogPost');
 
-describe('Associations', ()=>{
-  
-})
+describe('Associations', () => {
+  let joe, blogPost, comment;
+  beforeEach(done => {
+    joe = new User({ name: 'Joe' });
+    blogPost = new BlogPost({
+      title: 'JS is great',
+      content: 'reeeeeaaally great'
+    });
+    comment = new Comment({ content: 'Yeah sure' });
+
+    joe.blogPosts.push(blogPost);
+    blogPost.comments.push(comment);
+    comment.user = joe;
+
+    Promise.all([joe.save(), blogPost.save(), comment.save()]).then(() =>
+      done()
+    );
+  });
+
+  it.only('saves a relation between a user and a blogpost', done => {
+    User.findOne({ name: 'Joe' }).then(user => {
+      console.log(user);
+      done();
+    });
+  });
+});
